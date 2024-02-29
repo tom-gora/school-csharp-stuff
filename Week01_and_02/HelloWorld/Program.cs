3b898d74b12b86c963333aff102a0b3cf527c732
tom-gora <goratomasz@outlook.com>
2024-02-29 15:56:41 +0000

    first push

---
+++ Week01_and_02/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HelloWorld {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World from C#!");
+      Console.Write("Press any key to exit...");
+      Console.ReadKey();
+    }
+  }
+}
---
+++ Week03/MovieDatabaseApp-MySQL/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DataOperations;
+using UtilsCollection;
+
+namespace WebflixDatabase {
+  class Program {
+    static void Main(string[] args) {
+      // calling my util functions
+      Utils.PrintRandomMovie(Utils.GetRandomMovie());
+    }
+  }
+}
+
