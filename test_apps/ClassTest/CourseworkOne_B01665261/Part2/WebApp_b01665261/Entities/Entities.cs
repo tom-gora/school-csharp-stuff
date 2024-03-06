d4580cc8a4a1fd4cc6bb5dfcfd72b42983f3487a
tom-gora <goratomasz@outlook.com>
2024-03-06 15:59:07 +0000

    test applications built during an in class test

---
+++ test_apps/ClassTest/CourseworkOne_B01665261/Part2/WebApp_b01665261/Entities/Entities.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Entities {
+  public class Site {
+    public int ID { get; set; }
+    public string? SiteName { get; set; }
+    public string? URL { get; set; }
+  }
+}
